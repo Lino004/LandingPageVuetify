1608ef56d324f629ab97354ee32bd9f177c40997
Lino004 <h.ayinlamouhayad@gmail.com>
2018-11-21 19:35:20 +0100

    mise en place de l'effet parallax avec le deplacement scroll

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,59 +1,13 @@
 <template>
-    <v-content id="parallax" @scroll.native="scroll()" style="background-position-y: 0">
-        <div id="overlay__parallax"></div>
-        <div class="position">
-            <h1 class="title" :class="color">
-                Hello World
-            </h1>
-        </div>
-    </v-content>
+    
 </template>
 
 <script>
 export default {
-    data (){
-        return{
-            color: 'white'
-        }
-    },
-    methods : {
-        scroll () {
-            this.color = 'blue'
-            var elem = document.getElementById('parallax')
-            elem.style.backgroundPositionY = '-' + window.scrollY + 'px'
-        }
-    },
-    created (){
-        window.addEventListener("scroll", this.scroll)
-    },
-    destroyed (){
-        window.removeEventListener("scroll", this.scroll)
-    }
-};
+
+}
 </script>
 
 <style>
-#overlay__parallax{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    opacity: .6;
-    background-color: #000000;
-    z-index: 0;
-}
-#parallax{
-    margin: 0;
-    padding: 0;
-    background: url("../assets/hero-bg.jpg") no-repeat fixed;
-    background-size: cover;
-    height: 100%;
-}
-.position{
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-}
+
 </style>
