7f9a1359091affb472605d2a2f2f6c546f18f80d
keffa98 <a.fonton@epmistes.net>
2019-01-10 00:26:28 +0100

    <<Effet Overlay sur Images>>
    A revoir , le code bug.
    
    AROS
    
    Signed-off-by: keffa98 <a.fonoton@epmistes.net>

---
+++ src/tests/overlayeffect/src/index.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        active: false
+
+    },
+    methods: {
+        mouseOver: function () {
+            this.active = !this.active;
+
+
+        }
+    }
+});
+
+
